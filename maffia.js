20cd1dbc54b25b40790062ff97ef9d665f8c04b5
LinaLin123 <jlina.lin@gmail.com>
2020-11-19 13:26:04 +0100

    new pic styling

--- maffia.js
+++ maffia.js
@@ -1,16 +1,42 @@
-function card(name, description, image) {
+function card(name, role, description, image) {
   this.name = name;
+  this.role = role;
   this.description = description;
   this.image = image;
 }
 
 let deck = [
-  new card("Jimmy", "Bybor", "jimmy1"),
-  new card("Peter", "Maffia", "peter1"),
-  new card("Hans", "Doktor", "hans1"),
-  new card("Tommy", "Polis", "tommy1"),
-  new card("Martin", "Maffia", "martin1"),
-  new card("Sahir", "Maffia", "sahir1"),
+  new card("Fredrik ", "Bybor", "Har ingen funktion under natten", "fredrik"),
+  new card(
+    "Joakim ",
+    "Maffia",
+    "Väljer en person att döda under natten.",
+    "joakim"
+  ),
+  new card(
+    "Sabina ",
+    "Doktor",
+    "Väljer en person att rädda under natten. ",
+    "sabina"
+  ),
+  new card(
+    "Mia ",
+    "Polis",
+    "Varje natt kan sheriffen undersöka en av spelarna",
+    "mia"
+  ),
+  new card(
+    "Fredrik ",
+    "Kärleksparet",
+    "Båda personer dör om en av de dör",
+    "fredrikh"
+  ),
+  new card(
+    "Mimmi ",
+    "Skyddsängeln",
+    "Skyddsängeln får reda på vilka utav spelarna som är maffia.",
+    "mimmi"
+  ),
 ];
 
 function popRandom(deck) {
@@ -24,11 +50,13 @@ document.getElementById("draw").onclick = function () {
   console.log(index);
 
   document.getElementById("display").innerHTML =
-    '<img src="images/' +
+    '<div class="item-wrapper"><div class="item-card"> <div class="item-image item-image-cartoon"><img src="images/' +
     index.image +
-    '.png"><h2>' +
+    '.png"></div> <div class="item-name"><h2>' +
     index.name +
-    "</h2><h2>" +
+    "</h2></div> <div class='item-role'><h2>" +
+    index.role +
+    "</h2> </div> <div class='item-description'><p>" +
     index.description +
-    "</h2>";
+    "</p></div></div>";
 };
